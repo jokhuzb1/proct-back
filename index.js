71151598cb5801754275ed3b144a7a0ec4123545
jokhuzb1 <61691748+jokhuzb1@users.noreply.github.com>
2022-07-27 16:36:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ import database from './db/db.js'
 
 const app = express();
 app.use(express.json());
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 let db
 database.connect((err) => {
   if (!err) {
